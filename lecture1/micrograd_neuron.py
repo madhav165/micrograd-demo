2d2933ac0c213b8e929c6823a79176294952b07b
Madhav Kandukuri <Madhav.Kandukuri@ibm.com>
2022-09-29 16:33:30 +0530

    first lecture

--- lecture1/micrograd_neuron.py
+++ lecture1/micrograd_neuron.py
@@ -29,7 +29,10 @@ x1w1x2w2 = x1w1 + x2w2; x1w1x2w2.label = 'x1*w1 + x2*w2'
 n = x1w1x2w2 + b; n.label='n'
 o = n.tanh(); o.label='o'
 
+# set to intialize the _backward() correctly at output
 o.grad = 1.0
+
+# calculating the grad one node at a time in reverse order
 o._backward()
 n._backward()
 x1w1x2w2._backward()
