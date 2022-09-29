89ade76b4ede5f427ba91c913043a8428b07ca73
Madhav Kandukuri <Madhav.Kandukuri@ibm.com>
2022-09-29 13:59:29 +0530

    first lecture

--- lecture1/derivative_f_abc.py
+++ lecture1/derivative_f_abc.py
@@ -16,10 +16,10 @@ h = 1e-9
 d1 = a * b + c
 
 d2 = (a + h) * b + c
-print(f'derivative of d w.r.t a at a={a} = {(d2-d1)/h}')
+logging.info(f'derivative of d w.r.t a at a={a} = {(d2-d1)/h}')
 
 d2 = a * (b + h) + c
-print(f'derivative of d w.r.t b at b={b} = {(d2-d1)/h}')
+logging.info(f'derivative of d w.r.t b at b={b} = {(d2-d1)/h}')
 
 d2 = a * b + (c + h)
-print(f'derivative of d w.r.t c at c={c} = {(d2-d1)/h}')
+logging.info(f'derivative of d w.r.t c at c={c} = {(d2-d1)/h}')
