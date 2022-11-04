04c981f068da050578db0b2edd0cf5f04feba696
Madhav Kandukuri <Madhav.Kandukuri@ibm.com>
2022-11-04 14:30:49 +0530

    Create neuron.py

---
+++ lecture1/neuron.py
@@ -0,0 +1,14 @@
+import random
+from value import Value
+
+class Neuron:
+
+    def __init__(self, nin):
+        self.w = [Value(random.uniform(-1, 1)) for _ in range(nin)]
+        self.b = Value(random.uniform(-1, 1))
+
+    def __call__(self, x):
+        # w * x + b
+        act = sum((wi * xi for wi, xi in zip(self.w, x)), self.b)
+        out = act.tanh()
+        return out
